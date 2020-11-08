bf4d32d06a19c3bf0514f805f4960b2e8da1a3b6
Hui Zhu <teawater@antfin.com>
2020-11-08 18:55:10 +0800

    Add is_hugetlbfs() to GuestMemoryRegion
    Virtio-balloon can release the unused host memory to decrease the memory
    usage of the VMM.
    Release normal pages and hugetlbfs pages require different operations.
    (madvise MADV_DONTNEED and fallocate64 FALLOC_FL_PUNCH_HOLE)
    
    This commit add Add is_hugetlbfs() to GuestMemoryRegion to help
    VMM decide if this is a hugetlbfs address or not.
    
    Signed-off-by: Hui Zhu <teawater@antfin.com>

--- src/mmap_unix.rs
+++ src/mmap_unix.rs
@@ -90,6 +90,7 @@ pub struct MmapRegion {
     prot: i32,
     flags: i32,
     owned: bool,
+    hugetlbfs: bool,
 }
 
 // Send and Sync aren't automatically inherited for the raw address pointer.
@@ -172,6 +173,7 @@ impl MmapRegion {
             prot,
             flags,
             owned: true,
+            hugetlbfs: false,
         })
     }
 
@@ -213,6 +215,7 @@ impl MmapRegion {
             prot,
             flags,
             owned: false,
+            hugetlbfs: false,
         })
     }
 
@@ -271,6 +274,16 @@ impl MmapRegion {
             }
         }
         false
+    }
+
+    /// Set the hugetlbfs of the region
+    pub fn set_hugetlbfs(&mut self, hugetlbfs: bool) {
+        self.hugetlbfs = hugetlbfs
+    }
+
+    /// Returns `true` if the region is hugetlbfs
+    pub fn is_hugetlbfs(&self) -> bool {
+        self.hugetlbfs
     }
 }
 
@@ -352,6 +365,24 @@ mod tests {
             r.flags(),
             libc::MAP_ANONYMOUS | libc::MAP_NORESERVE | libc::MAP_PRIVATE
         );
+    }
+
+    #[test]
+    fn test_mmap_region_set_hugetlbfs() {
+        assert!(MmapRegion::new(0).is_err());
+
+        let size = 4096;
+
+        let mut r = MmapRegion::new(4096).unwrap();
+        r.set_hugetlbfs(true);
+        assert_eq!(r.size(), size);
+        assert!(r.file_offset().is_none());
+        assert_eq!(r.prot(), libc::PROT_READ | libc::PROT_WRITE);
+        assert_eq!(
+            r.flags(),
+            libc::MAP_ANONYMOUS | libc::MAP_NORESERVE | libc::MAP_PRIVATE
+        );
+        assert_eq!(r.is_hugetlbfs(), true);
     }
 
     #[test]
